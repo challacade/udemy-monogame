c17b300d795bee6cad89a293c3101ab355c5d164
Kyle Schaub <kylejschaub@gmail.com>
2018-02-01 19:59:55 -0600

    Uploaded Spaceship project files

---
+++ Spaceship/Controller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Spaceship
+{
+    class Controller
+    {
+        public List<Asteroid> asteroids = new List<Asteroid>();
+        public double timer = 2D;
+        public double maxTime = 2D;
+        public int nextSpeed = 240;
+        public float totalTime = 0f;
+
+        public bool inGame = false;
+
+        public void conUpdate(GameTime gameTime) {
+            if (inGame)
+            {
+                timer -= gameTime.ElapsedGameTime.TotalSeconds;
+                totalTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            else
+            {
+                KeyboardState kState = Keyboard.GetState();
+                if (kState.IsKeyDown(Keys.Enter)) {
+                    inGame = true;
+                    totalTime = 0f;
+                    timer = 2D;
+                    maxTime = 2D;
+                    nextSpeed = 240;
+                }
+            }
+
+            if (timer <= 0) {
+                asteroids.Add(new Asteroid(nextSpeed));
+                timer = maxTime;
+                if (maxTime > 0.5)
+                {
+                    maxTime -= 0.1D;
+                }
+                if (nextSpeed < 720)
+                {
+                    nextSpeed += 4;
+                }
+            }
+        }
+    }
+}
