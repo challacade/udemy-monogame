fe9d07fffa458331672732bba83e9714df8e41e2
Kyle Schaub <kylejschaub@gmail.com>
2018-02-01 20:03:59 -0600

    Upload RPG project files

---
+++ RPG/Enemy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RPG
+{
+    class Enemy
+    {
+        private Vector2 position;
+        protected int health;
+        protected int speed;
+        protected int radius;
+
+        public static List<Enemy> enemies = new List<Enemy>();
+
+        public int Health {
+            get { return health; }
+            set { health = value; }
+        }
+
+        public Vector2 Position {
+            get { return position; }
+        }
+
+        public int Radius
+        {
+            get { return radius; }
+        }
+
+        public Enemy(Vector2 newPos) {
+            position = newPos;
+        }
+
+        public void Update(GameTime gameTime, Vector2 playerPos) {
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            Vector2 moveDir = playerPos - position;
+            moveDir.Normalize();
+
+            Vector2 tempPos = position;
+            tempPos += moveDir * speed * dt;
+            if (!Obstacle.didCollide(tempPos, radius)) {
+                position += moveDir * speed * dt;
+            }
+        }
+
+        public static void SpawnEnemies() {
+            Enemy.enemies.Add(new Snake(new Vector2(552, 1412)));
+            Enemy.enemies.Add(new Eye(new Vector2(927, 103)));
+        }
+    }
+
+    class Snake : Enemy {
+        public Snake(Vector2 newPos) : base(newPos) {
+            speed = 110;
+            radius = 42;
+            health = 3;
+        }
+    }
+
+    class Eye : Enemy {
+        public Eye(Vector2 newPos) : base(newPos) {
+            speed = 80;
+            radius = 45;
+            health = 5;
+        }
+    }
+}
---
+++ RPG/Projectile.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RPG
+{
+    class Projectile
+    {
+        private Vector2 position;
+        private int speed = 800;
+        private int radius = 15;
+        private Dir direction;
+        private bool collided = false;
+
+        public static List<Projectile> projectiles = new List<Projectile>();
+
+        public Projectile(Vector2 newPos, Dir newDir) {
+            position = newPos;
+            direction = newDir;
+        }
+
+        public bool Collided {
+            get { return collided; }
+            set { collided = value; }
+        }
+
+        public Vector2 Position {
+            get {
+                return position;
+            }
+        }
+
+        public int Radius {
+            get {
+                return radius;
+            }
+        }
+
+        public void Update(GameTime gameTime) {
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            switch (direction)
+            {
+                case Dir.Right:
+                    position.X += speed * dt;
+                    break;
+                case Dir.Left:
+                    position.X -= speed * dt;
+                    break;
+                case Dir.Down:
+                    position.Y += speed * dt;
+                    break;
+                case Dir.Up:
+                    position.Y -= speed * dt;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
